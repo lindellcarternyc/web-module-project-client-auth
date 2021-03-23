5624ac3d694bff527f22d1359bd6be9cfbd3dc8a
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-23 13:45:26 -0400

    add get friends request

--- friends/src/api/index.js
+++ friends/src/api/index.js
@@ -2,12 +2,32 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:6969/api'
 
+const axiosWithAuth = () => {
+  const token = window.localStorage.getItem('friendsListToken')
+  console.log(token)
+  return axios.create({
+    headers: {
+      'authorization': token
+    }
+  })
+}
+
 export const login = async ({ username, password }) => {
   try {
     const res = await axios.post(`${BASE_URL}/login`, { username, password })
     const token = res.data.payload
     console.log('token', token)
     window.localStorage.setItem('friendsListToken', token)
+  } catch (err) {
+    throw err
+  }
+}
+
+export const getFriends = async () => {
+  try {
+    const response = await axiosWithAuth().get(`${BASE_URL}/friends`)
+    console.log(response.data)
+    return response.data
   } catch (err) {
     throw err
   }
--- friends/src/pages/FriendsListPage.js
+++ friends/src/pages/FriendsListPage.js
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react'
+
+import * as api from '../api'
+
 const FriendsListPage = (props) => {
+  const [friends, setFriends] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  console.log(friends)
+  useEffect(() => {
+    setIsLoading(true)
+    api.getFriends()
+      .then(data => {
+        setFriends(data)
+        setIsLoading(false)
+      })
+      .catch(err => {
+        setIsLoading(false)
+        setError(err)
+      })  
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (error !== null) {
+    return <p>{JSON.stringify(error)}</p>
+  }
+
   return (
-    <div>Friends List Page</div>
+    <div>
+      <h2>Friends List</h2>
+      <div>
+        {friends.map(f => {
+          return <p key={f.id}>{f.name}</p>
+        })}
+      </div>
+    </div>
   )
 }
 
